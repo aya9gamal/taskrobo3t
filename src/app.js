01d4e3a5b1be0e7e4512e0333c817a74b918cb43
unknown <{ID}+{username}@users.noreply.github.com>
2024-12-09 18:24:50 +0200

    first2 commit

--- src/app.js
+++ src/app.js
@@ -10,85 +10,86 @@ mongoclient.connect(connectionUrl, (error, res1) => {
   }
   const db = res1.db(dbname);
   //// insert one
-//   db.collection('users').insertOne({
-//       name : 'aya',
-//       age :25,
-//       city :'assuit'
-//   },(error,data)=>{
-//       if (error){
-//           console.log(error)
-//       }
-//       console.log(data)
-//   })
+  db.collection('users').insertOne({
+      name : 'aya',
+      age :25,
+      city :'assuit'
+  },(error,data)=>{
+      if (error){
+          console.log(error)
+      }
+      console.log(data)
+  })
 
-//   db.collection('users').insertOne({
-//       name : "asmaa",
-//       age :26,
-//       city : 'cairo'
-//   },(error,data)=>{
-//       if (error){
-//           console.log(error)
-//       }
-//       console.log(data)
-//   })
+  db.collection('users').insertOne({
+      name : "asmaa",
+      age :26,
+      city : 'cairo'
+  },(error,data)=>{
+      if (error){
+          console.log(error)
+      }
+      console.log(data)
+  })
 
 
 
   /////insertmany
-//   db.collection('users').insertMany([
-//       {
-//           name : 'fatma',
-//           age : 21,
-//           city : 'Giza'
+  db.collection('users').insertMany([
+      {
+          name : 'fatma',
+          age : 21,
+          city : 'Giza'
 
-//       },
-//       {
-//           name : 'mona',
-//           age :22,
-//           city : 'mina'
-//       },
-//       {
-//           name : 'omar',
-//           age :23,
-//           city : 'assuit'
-//       },
-//       {
-//           name : 'ahmed',
-//           age :24,
-//           city : 'Cairo'
-//       },
-//       {
-//           name : 'amr',
-//           age :25,
-//           city : 'giza'
-//       },
-//       {
-//           name : 'mona',
-//           age :27,
-//           city : 'Cairo'
-//       },
-//       {
-//           name : 'gamal',
-//           age :27,
-//           city : 'Assuit'
-//       },
-//       {
-//           name : 'Mohmed',
-//           age :27,
-//           city : 'Assuit'
-//       },
-//       {
-//           name : 'ali',
-//           age :27,
-//           city : 'Assuit'
-//       },
-//       {
-//           name : 'rabab',
-//           age :27,
-//           city : 'Assuit'
-//       }
-//   ]).then((data)=>{console.log(data.insertedCount)})
-//     .catch((error)=>{console.log(error)})
+      },
+      {
+          name : 'mona',
+          age :22,
+          city : 'mina'
+      },
+      {
+          name : 'omar',
+          age :23,
+          city : 'assuit'
+      },
+      {
+          name : 'ahmed',
+          age :24,
+          city : 'Cairo'
+      },
+      {
+          name : 'amr',
+          age :25,
+          city : 'giza'
+      },
+      {
+          name : 'mona',
+          age :27,
+          city : 'Cairo'
+      },
+      {
+          name : 'gamal',
+          age :27,
+          city : 'Assuit'
+      },
+      {
+          name : 'Mohmed',
+          age :27,
+          city : 'Assuit'
+      },
+      {
+          name : 'ali',
+          age :27,
+          city : 'Assuit'
+      },
+      {
+          name : 'rabab',
+          age :27,
+          city : 'Assuit'
+      }
+  ]).then((data)=>{console.log(data.insertedCount)})
+    .catch((error)=>{console.log(error)})
+
 
   // /// find age 27
      db.collection('users').find({age : 27}).toArray((error,data)=>{
@@ -98,8 +99,7 @@ mongoclient.connect(connectionUrl, (error, res1) => {
       console.log(data)
      })
 
-  //fin first 3
-
+  /////fin first 3
   db.collection('users').find({age : 27}).limit(3).toArray((error,data)=>{
       if ( error ){
           console.log("error ")
@@ -110,38 +110,47 @@ mongoclient.connect(connectionUrl, (error, res1) => {
   //////upadat
 
   
-// db.collection('users').find().limit(4).forEach(
-//     (doc)=>{
-//         db.collection('users').updateMany({
-//             _id : doc._id
-//         },{
-//             $set:{name : "anor"},
+db.collection('users').find().limit(4).forEach(
+    (doc)=>{
+        db.collection('users').updateMany({
+            _id : doc._id
+        },{
+            $set:{name : "nagy"},
            
-//         })
-//     }
-// )
+        })
+    }
+)
+// ///updateone $inc age 5
+db.collection('users').updateOne({
+  name : "omar"
+},{
+  $inc : {age :5}
+}).then((data)=>{console.log(data.modifiedCount)})
+.catch((error)=>{console.log(error)})
 
-// $inc firt four age +10
-// db.collection('users').find({age:27}).limit(4).forEach(
-//     (doc)=>{
-//         db.collection('users').updateMany({
-//             _id : doc._id
-//         },{
-//             $inc :{age : 10} 
+
+///// $inc firt four age +10
+db.collection('users').find({age:27}).limit(4).forEach(
+    (doc)=>{
+        db.collection('users').updateMany({
+            _id : doc._id
+        },{
+            $inc :{age : 10} 
            
-//         })
-//     }
-// )
+        })
+    }
+)
 
-// $inc all +10
-//  db.collection('users').updateMany({},{
-//     $inc : {age :10}
-//  }).then((data)=>{console.log(data.modifiedCount)})
-//  .catch((error)=>{console.log(error)})
+//// $inc all +10
+ db.collection('users').updateMany({},{
+    $inc : {age :10}
+ }).then((data)=>{console.log(data.modifiedCount)})
+ .catch((error)=>{console.log(error)})
 
-// delate many age :47
-// db.collection('users').deleteMany({age : 47})
-// .then((data)=>{console.log(data.deletedCount)})
-// .catch((error)=>{console.log(error)})
+// ///delate many age :47
+
+db.collection('users').deleteMany({age : 47})
+.then((data)=>{console.log(data.deletedCount)})
+.catch((error)=>{console.log(error)})
 
 });
